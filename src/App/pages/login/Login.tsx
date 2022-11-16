c2a3bbbd76285f27040ba2b3f673c65dfa0302a5
Geraldo Nunes <geraldojuniorgj@hotmail.com>
2022-11-16 16:36:34 -0300

    useMemo

--- src/App/pages/login/Login.tsx
+++ src/App/pages/login/Login.tsx
@@ -1,26 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-useEffect(() => {
+    const emailLength = useMemo(() => {
+        console.log('Executou')
+    return email.length*1000;
+    },[email.length]);
+
+    useEffect(() => {
     console.log(email)
     },[email]);
+
     useEffect(() => {
         console.log(password)  
         },[password]);
-
 
     const handleEntrar = () => {
         console.log(email)
         console.log(password)
     }
 
-
     return (
        <div>
         <form>
+<p>Quantidade de caracteres no email: {emailLength}</p>
 
             <label>
                 <span>Email</span>
